d95006884bb4b2a5bea2813017beb66f2edbee24
Rafaelsoso <anhnguyenmta1318@gmail.com>
2024-07-23 14:11:13 +0700

    improve textview

--- resizableTextView-SwiftUI/ContentView.swift
+++ resizableTextView-SwiftUI/ContentView.swift
@@ -40,50 +40,53 @@ struct Home: View {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack {
                     // Chat content
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("xinchao")
-                    Text("dsjfljsfjajdfajsdljaskdjklasjdlkajsdkljasdkljadksjaklsdjlkasjdklasjdljsaldjasjdlasjdlajsdjklsjdklasjdljasdjlasjdlajsdjasjdklajsdljaksjdlasjdljsadjksadjlasjdlsajdlajsdkjasasdjlsajdlksjkdljasdjasjdklasjdklsajdkajskldjasjdkasjdljasdjsldjlsajdlasjdjsaldjlsdjlasjdaksjdlsjdlajdlsjdljasdjlasjdkasjdl")
-                }
-            }
-
-            HStack(spacing: 8) {
-                ResizableTF(txt: $txt, height: $height)
-                    .frame(height: self.height < 150 ? self.height : 150)
-                    .padding(.horizontal)
-                    .background(Color.white)
-                    .cornerRadius(15.0)
-                    .onTapGesture {
+                    ForEach(Array(0...20).indices, id: \.self){ _ in
+                        Comment()
                     }
-                
-                Button {
-                    
-                } label: {
-                    Image("send")
-                        .resizable()
-                        .frame(width: 24, height: 20)
-                        .foregroundColor(.black)
-                        .padding(13)
                 }
-                .background(.white)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-
             }
-            .padding(.horizontal)
+            .padding(.horizontal, 12)
+
+            VStack {
+                HStack {
+                    ResizableTF(txt: $txt, height: $height)
+                        .frame(height: self.height < 150 ? self.height : 150)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(5.0)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5.0)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        .onTapGesture {
+                        }
+                }
+                .padding(.horizontal, 12)
+                .padding(.vertical, 8)
+            }
+            HStack {
+                HStack {
+                    Text("[投稿ガイドライン](myappurl://action)").underline() +
+                    Text("に関するテキスト")
+                }.onOpenURL { link in
+
+                }
+                Spacer()
+                HStack {
+                    Text("00/000")
+                    Button {
+                        // action
+                    } label: {
+                        Text("投稿")
+                            .frame(width: 64, height: 36)
+                            .foregroundColor(.white)
+                            .padding(13)
+                    }
+                    .background(.blue)
+                    .cornerRadius(3)
+                }
+            }
+            .padding(.horizontal, 12)
         }
         .padding(.bottom, self.keyboardHeight)
         .background(Color.black.opacity(0.06).edgesIgnoringSafeArea(.bottom))
@@ -102,6 +105,26 @@ struct Home: View {
     }
 }
 
+
+struct Comment: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("9999")
+            Text("コメントテキスト文章文章文章コメントテキスト文章文章文章コメントテキスト文章文章文章コメントテキスト文章文章文章コメントテキスト文章文章文章")
+            HStack {
+                Text("0000-00-00 00:00:00:00")
+                Text("通報")
+                Spacer()
+                Image("clear")
+                Text("876")
+            }
+            Rectangle()
+                .frame(height: 4)
+                .background(.gray)
+        }
+    }
+}
+
 struct ResizableTF: UIViewRepresentable {
     
     @Binding var txt: String
@@ -115,10 +138,22 @@ struct ResizableTF: UIViewRepresentable {
         let view = UITextView()
         view.isEditable = true
         view.isScrollEnabled = true
-        view.text = "Enter Message"
         view.font = .systemFont(ofSize: 18)
         view.textColor = .gray
         view.backgroundColor = .clear
+
+        let placeholderLabel = UILabel()
+        placeholderLabel.text = "コメント入力に関するプレースホルダー"
+        if let font = view.font {
+            placeholderLabel.font = .italicSystemFont(ofSize: font.pointSize)
+            placeholderLabel.frame.origin = CGPoint(x: 5, y: font.pointSize / 2)
+        }
+        placeholderLabel.sizeToFit()
+        view.addSubview(placeholderLabel)
+        placeholderLabel.textColor = .tertiaryLabel
+        placeholderLabel.isHidden = !view.text.isEmpty
+        placeholderLabel.tag = 1
+
         view.delegate = context.coordinator
         return view
     }
@@ -137,9 +172,9 @@ struct ResizableTF: UIViewRepresentable {
         }
         
         func textViewDidBeginEditing(_ textView: UITextView) {
-            if self.parent.txt == "" {
-                textView.text = ""
-                textView.textColor = .black
+            textView.textColor = .black
+            if let placeholder = textView.subviews.first(where: { $0.tag == 1 }) {
+                placeholder.isHidden = !textView.text.isEmpty
             }
         }
         
@@ -147,6 +182,15 @@ struct ResizableTF: UIViewRepresentable {
             DispatchQueue.main.async {
                 self.parent.height = textView.contentSize.height
                 self.parent.txt = textView.text
+                if let placeholder = textView.subviews.first(where: { $0.tag == 1 }) {
+                    placeholder.isHidden = !textView.text.isEmpty
+                }
+            }
+        }
+        
+        func textViewDidEndEditing(_ textView: UITextView) {
+            if let placeholder = textView.subviews.first(where: { $0.tag == 1 }) {
+                placeholder.isHidden = !textView.text.isEmpty
             }
         }
     }
